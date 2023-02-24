a11b042f2c9e983ad23bfa8b5cc640c031ca00f3
Romain Ballesteros <romain.ballesteros@gmail.com>
2023-02-24 08:15:25 +0100

    main creation

--- ecommerce-product-page-main 2/script.js
+++ ecommerce-product-page-main 2/script.js
@@ -1,44 +1,25 @@
-let modal = null;
-const openModal = function (e) {
-  e.preventDefault();
-  const target = document.querySelector(e.target.getAttribute("href"));
-  target.style.display = null;
-  target.removeAttribute("aria-hidden");
-  target.setAttribute("aria-modal", "true");
-  modal = target;
-  modal.addEventListener("click", closemodal);
-  modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
-  modal
-    .querySelector(".js-modal-stop")
-    .addEventListener("click", stopPropagation);
-};
-const closeModal = function (e) {
-  if (modal === null) return;
-  e.preventDefault();
-  modal.style.display = none;
-  modal.setAttribute("aria-hidden", "true");
-  modal.removeAttribute("aria-modal");
-  modal = target;
-  modal.removeListener("click", closemodal);
-  modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
-  modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
-  modal
-    .querySelector(".js-modal-close")
-    .removeEventListener("click", closeModal);
-  modal
-    .querySelector(".js-modal-stop")
-    .removeEventListener("click", stopPropagation);
-  modal = null;
-};
-const stopPropagation = function (e) {
-  e.stopPropagation();
-};
-document.querySelectorAll(".js-modal").forEach((a) => {
-  a.addEventListener.addEventListener("click", openModal);
-});
+// Récupère le bouton pour ouvrir le modal
+var boutonModal = document.getElementById("ouvrirModal");
 
-window.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" || e.key === "Esc") {
-    closeModal(e);
+// Récupère le modal
+var modal = document.getElementById("monModal");
+
+// Récupère le span "fermer"
+var spanFermer = document.getElementsByClassName("fermer")[0];
+
+// Ouvre le modal lorsque l'utilisateur clique sur le bouton
+boutonModal.onclick = function () {
+  modal.style.display = "block";
+};
+
+// Ferme le modal lorsque l'utilisateur clique sur le span "fermer"
+spanFermer.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Ferme le modal lorsque l'utilisateur clique en dehors du modal
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-});
+};
